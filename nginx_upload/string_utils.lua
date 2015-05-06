dd43935b0edb705a7542896ba8b934ca9e88f50e
Flavio Grossi <flaviogrossi@gmail.com>
2015-05-06 12:53:19 +0200

    add string utility module

---
+++ nginx_upload/string_utils.lua
@@ -0,0 +1,36 @@
+local M = {}
+
+
+function M.split(s, sSeparator, nMax, bRegexp)
+    -- split the string s using the separator s. Returns the list of splits
+    -- code inspired by http://lua-users.org/wiki/StringRecipes
+    assert(sSeparator ~= '')
+    assert(nMax == nil or nMax >= 1)
+
+    if s == '' then
+        return {''}
+    end
+
+    local aRecord = {}
+
+    if s:len() > 0 then
+        local bPlain = not bRegexp
+        nMax = nMax or -1
+
+        local nField=1 nStart=1
+        local nFirst,nLast = s:find(sSeparator, nStart, bPlain)
+        while nFirst and nMax ~= 0 do
+            aRecord[nField] = s:sub(nStart, nFirst-1)
+            nField = nField+1
+            nStart = nLast+1
+            nFirst,nLast = s:find(sSeparator, nStart, bPlain)
+            nMax = nMax-1
+        end
+        aRecord[nField] = s:sub(nStart)
+    end
+
+    return aRecord
+end
+
+
+return M
