36940c0ce23b20af19c0da3acc25b6f6aa3d5963
Flavio Grossi <flaviogrossi@gmail.com>
2015-05-06 15:02:22 +0200

    add tests for enumerate_from_string_range function

--- tests/test_string_utils.lua
+++ tests/test_string_utils.lua
@@ -8,6 +8,15 @@ local print = print
 module("p", lunit.testcase)
 
 
+function assert_table_length(expected_len, t, message)
+    local count = 0
+    for i, v in pairs(t) do
+        count = count + 1
+    end
+    return lunit.assert_equal(expected_len, count, message)
+end
+
+
 function test_basic_split()
     local t = string_utils.split('one,two,three', ',')
     local expected = {'one', 'two', 'three'}
@@ -74,4 +83,50 @@ end
 function test_strip_with_empty_input()
     local s = string_utils.strip('')
     lunit.assert_equal('', s)
+end
+
+function test_basic_enumerate_from_string_range()
+    local t = string_utils.enumerate_from_string_range('10,20,30-32')
+    local expected = {10, 20, 30, 31, 32}
+
+    assert_table_length(#expected, t)
+    for _, v in pairs(expected) do
+        lunit.assert_true(t[v])
+    end
+
+    local t = string_utils.enumerate_from_string_range('30-32,10,20')
+    local expected = {10, 20, 30, 31, 32}
+
+    assert_table_length(#expected, t)
+    for _, v in pairs(expected) do
+        lunit.assert_true(t[v])
+    end
+end
+
+function test_enumerate_from_string_range_invalid_returns_empty()
+    assert_table_length(0, string_utils.enumerate_from_string_range(''))
+    assert_table_length(0, string_utils.enumerate_from_string_range('a'))
+    assert_table_length(0,
+                        string_utils.enumerate_from_string_range('10+12+13'))
+    assert_table_length(0,
+                        string_utils.enumerate_from_string_range('13-w'))
+end
+
+function test_enumerate_from_string_range_only_single_els()
+    local t = string_utils.enumerate_from_string_range('10,20,30')
+    local expected = {10, 20, 30}
+
+    assert_table_length(#expected, t)
+    for _, v in pairs(expected) do
+        lunit.assert_true(t[v])
+    end
+end
+
+function test_enumerate_from_string_range_invalid_range_is_ignored()
+    local t = string_utils.enumerate_from_string_range('10,13-12')
+    local expected = {10}
+    assert_table_length(#expected, t)
+    for _, v in pairs(expected) do
+        lunit.assert_true(t[v])
+    end
 end
